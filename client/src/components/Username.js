d3b0d44fc365938333582960491b2e988b787106
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-13 13:26:31 +0530

    formik + toaster

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import avatar from '../assets/profile.png'
-
+import { Toaster} from 'react-hot-toast'
+import {useFormik } from 'formik'
 import styles from '../styles/Username.module.css'
+import { usernameValidate } from '../helper/validate'
 
 export const Username = () => {
+  const formik = useFormik({
+    initialValues: { username: '' },
+    validateOnBlur: false,
+    validateOnChange: false,
+
+    onSubmit: async values=>{
+      console.log(values)
+    }
+  })
+
+
   return (
     <div className="container mx-auto items-center">
+
+      <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col items-center">
@@ -16,13 +31,13 @@ export const Username = () => {
             </span>
           </div>
 
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} className = {styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input className={styles.textbox} type="text" placeholder='Username' />
+              <input {...formik.getFieldProps('username')} className={styles.textbox} type="text" placeholder='Username' />
               <button className = {styles.btn} type="submit">Let's Go</button>
             </div>
 
