ce092db482f4793231ef421c4e997c70dc65d181
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-13 07:12:23 +0530

    create routes
    all routes initialised

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,54 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Reset from './components/Reset';
+import PageNotFound from './components/PageNotFound';
 
-const App = () => {
+/**root routes**/
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Username></Username>
+  },
+  {
+    path: '/register',
+    element: <Register></Register>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '*',
+    element: <PageNotFound></PageNotFound>
+  },
+])
+
+
+export default function App() {
   return (
-    <h1>Hello</h1>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   )
 }
-
-export default App
