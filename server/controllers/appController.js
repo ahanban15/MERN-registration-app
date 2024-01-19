988a643eaf07493dd49ff28a6d438110e28f1199
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-19 21:43:29 +0530

    register

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,24 +1,79 @@
+import UserModel from '../model/User_model.js'
+import bcrypt from 'bcrypt'
+
 /** POST: http://localhost:8080/api/register 
  * @param : {
-  "username" : "example123",
-  "password" : "admin123",
-  "email": "example@gmail.com",
-  "firstName" : "bill",
-  "lastName": "william",
-  "mobile": 8009860560,
-  "address" : "Apt. 556, Kulas Light, Gwenborough",
-  "profile": ""
+    "username" : "example123",
+    "password" : "admin123",
+    "email": "example@gmail.com",
+    "firstName" : "bill",
+    "lastName": "william",
+    "mobile": 8009860560,
+    "address" : "Apt. 556, Kulas Light, Gwenborough",
+    "profile": ""
 }
 */
 export async function register(req, res){
-    res.join('register route')
+    // res.join('register route')
+    try{
+        const { username, password, profile, email } = req.body;
+
+        const existUsername = new Promise((resolve, reject)=>{
+            UserModel.findOne({ username }, function(err, user){
+                if(err) reject(new Error(err))
+
+                if(user) reject({ error: "Please use unique Username: "});
+
+                resolve();
+            })
+        });
+
+        const existEmail = new Promise((resolve, reject)=>{
+            UserModel.findOne({ email }, function(err, email){
+                if(err) reject(new Error(err))
+
+                if(user) reject({ error: "Please use unique Email: "});
+
+                resolve();
+            })        
+        });
+
+        Promise.all([existUsername, existEmail])
+            .then(()=>{
+                if(password){
+                    bcrypt.hash(password, 10)
+                        .then( hashedPassword => {
+                            const user = new UserModel({
+                                username,
+                                password: hashedPassword,
+                                profile: profile || '',
+                                email: email
+                            })
+
+                            user.save()
+                                .then(result => res.status(201).send({msg: "User Register Successfully"}))
+                                .catch(error => res.status(500).send({error}))
+                        }).catch(error=>{
+                            return res.status(500).send({
+                                error: "Enable to hashed password"
+                            })
+                        })
+                }
+            }).catch(error=>{
+                return res.status(500).send({
+                    error: "Enable to hashed password"
+                })
+            })
+    }catch(error){
+        return res.status(500).send(error);
+    }
 }
 
 
 /** POST: http://localhost:8080/api/login 
  * @param: {
-  "username" : "example123",
-  "password" : "admin123"
+    "username" : "example123",
+    "password" : "admin123"
 }
 */
 export async function login(req, res){
@@ -32,7 +87,7 @@ export async function getUser(req, res){
 
 /** PUT: http://localhost:8080/api/updateuser 
  * @param: {
-  "header" : "<token>"
+    "header" : "<token>"
 }
 body: {
     firstName: '',
