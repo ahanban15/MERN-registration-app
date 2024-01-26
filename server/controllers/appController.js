a82cdc3665e179c00cbc7b85851aefe243810964
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-26 12:16:22 +0530

    Update appController.js

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,25 @@
-import UserModel from '../model/User_model.js'
-import bcrypt from 'bcrypt'
+import UserModel from '../model/User.model.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js'
+import otpGenerator from 'otp-generator';
+
+/** middleware for verify user */
+export async function verifyUser(req, res, next){
+    try {
+        
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        // check the user existance
+        let exist = await UserModel.findOne({ username });
+        if(!exist) return res.status(404).send({ error : "Can't find User!"});
+        next();
+
+    } catch (error) {
+        return res.status(404).send({ error: "Authentication Error"});
+    }
+}
+
 
 /** POST: http://localhost:8080/api/register 
  * @param : {
@@ -72,22 +92,80 @@ export async function register(req, res){
 
 /** POST: http://localhost:8080/api/login 
  * @param: {
-    "username" : "example123",
-    "password" : "admin123"
+  "username" : "example123",
+  "password" : "admin123"
 }
 */
-export async function login(req, res){
-    res.join('login route')
+export async function login(req,res){
+   
+    const { username, password } = req.body;
+
+    try {
+        
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
+
+                        if(!passwordCheck) return res.status(400).send({ error: "Don't have Password"});
+
+                        // create jwt token
+                        const token = jwt.sign({
+                                        userId: user._id,
+                                        username : user.username
+                                    }, ENV.JWT_SECRET , { expiresIn : "24h"});
+
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });                                    
+
+                    })
+                    .catch(error =>{
+                        return res.status(400).send({ error: "Password does not Match"})
+                    })
+            })
+            .catch( error => {
+                return res.status(404).send({ error : "Username not Found"});
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error});
+    }
 }
 
+
 /** GET: http://localhost:8080/api/user/example123 */
-export async function getUser(req, res){
-    res.join('getUser route')
+export async function getUser(req,res){
+    
+    const { username } = req.params;
+
+    try {
+        
+        if(!username) return res.status(501).send({ error: "Invalid Username"});
+
+        UserModel.findOne({ username }, function(err, user){
+            if(err) return res.status(500).send({ err });
+            if(!user) return res.status(501).send({ error : "Couldn't Find the User"});
+
+            /** remove password from user */
+            // mongoose return unnecessary data with object so convert it into json
+            const { password, ...rest } = Object.assign({}, user.toJSON());
+
+            return res.status(201).send(rest);
+        })
+
+    } catch (error) {
+        return res.status(404).send({ error : "Cannot Find User Data"});
+    }
+
 }
+
 
 /** PUT: http://localhost:8080/api/updateuser 
  * @param: {
-    "header" : "<token>"
+  "header" : "<token>"
 }
 body: {
     firstName: '',
@@ -95,18 +173,47 @@ body: {
     profile : ''
 }
 */
-export async function updateUser(req, res){
-    res.join('updateUser route')
+export async function updateUser(req,res){
+    try {
+        
+        // const id = req.query.id;
+        const { userId } = req.user;
+
+        if(userId){
+            const body = req.body;
+
+            // update the data
+            UserModel.updateOne({ _id : userId }, body, function(err, data){
+                if(err) throw err;
+
+                return res.status(201).send({ msg : "Record Updated...!"});
+            })
+
+        }else{
+            return res.status(401).send({ error : "User Not Found...!"});
+        }
+
+    } catch (error) {
+        return res.status(401).send({ error });
+    }
 }
 
+
 /** GET: http://localhost:8080/api/generateOTP */
-export async function generateOTP(req, res){
-    res.join('generateOTP route')
+export async function generateOTP(req,res){
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false})
+    res.status(201).send({ code: req.app.locals.OTP })
 }
 
 /** GET: http://localhost:8080/api/verifyOTP */
-export async function verifyOTP(req, res){
-    res.join('verifyOTP route')
+export async function verifyOTP(req,res){
+    const { code } = req.query;
+    if(parseInt(req.app.locals.OTP) === parseInt(code)){
+        req.app.locals.OTP = null; // reset the OTP value
+        req.app.locals.resetSession = true; // start session for reset password
+        return res.status(201).send({ msg: 'Verify Successsfully!'})
+    }
+    return res.status(400).send({ error: "Invalid OTP"});
 }
 
 // successfully redirect user when OTP is valid
