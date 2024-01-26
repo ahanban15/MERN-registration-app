8f00e62f8a372be5dc6f112ef4de4eff679a5dee
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-26 14:01:22 +0530

    Update validate.js

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,57 +1,41 @@
 import toast from 'react-hot-toast'
+import { authenticate } from './helper'
 
-// validate login page username
+/** validate login page username */
 export async function usernameValidate(values){
     const errors = usernameVerify({}, values);
+
+    if(values.username){
+        // check user exist or not
+        const { status } = await authenticate(values.username);
+        
+        if(status !== 200){
+            errors.exist = toast.error('User does not exist...!')
+        }
+    }
 
     return errors;
 }
 
-// vaidate login page password
+/** validate password */
 export async function passwordValidate(values){
     const errors = passwordVerify({}, values);
 
     return errors;
 }
 
-// validate username
-function usernameVerify(error = {}, values){
-    if(!values.username){
-        error.username = toast.error("Username Required...!");
-    }else if(values.username.includes(" ")){
-        error.username = toast.error('Invalid Username...!');
-    }
-
-    return error;
-}
-
-// validate password
-function passwordVerify(errors ={}, values) {
-
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-
-    if(!values.password){
-        errors.password = toast.error("Password Required...!");
-    }else if (values.password.includes(" ")){
-        errors.password = toast.error("Wrong password...!");
-    }else if (values.password.length < 4){
-        errors.password = toast.error("Password must be more than 4 characters")
-    }else if(!specialChars.test(values.password)){
-        errors.password = toast.error("At least one special character is required");
-    }
-}
-
-// validate reset password
+/** validate reset password */
 export async function resetPasswordValidation(values){
     const errors = passwordVerify({}, values);
 
     if(values.password !== values.confirm_pwd){
-        errors.exist = toast.error("Password NOT match...!");
+        errors.exist = toast.error("Password not match...!");
     }
-} 
 
+    return errors;
+}
 
-// validate register form
+/** validate register form */
 export async function registerValidation(values){
     const errors = usernameVerify({}, values);
     passwordVerify(errors, values);
@@ -60,7 +44,46 @@ export async function registerValidation(values){
     return errors;
 }
 
-// validate email
+/** validate profile page */
+export async function profileValidation(values){
+    const errors = emailVerify({}, values);
+    return errors;
+}
+
+
+/** ************************************************* */
+
+/** validate password */
+function passwordVerify(errors = {}, values){
+    /* eslint-disable no-useless-escape */
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if(!values.password){
+        errors.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        errors.password = toast.error("Wrong Password...!");
+    }else if(values.password.length < 4){
+        errors.password = toast.error("Password must be more than 4 characters long");
+    }else if(!specialChars.test(values.password)){
+        errors.password = toast.error("Password must have special character");
+    }
+
+    return errors;
+}
+
+
+/** validate username */
+function usernameVerify(error = {}, values){
+    if(!values.username){
+        error.username = toast.error('Username Required...!');
+    }else if(values.username.includes(" ")){
+        error.username = toast.error('Invalid Username...!')
+    }
+
+    return error;
+}
+
+/** validate email */
 function emailVerify(error ={}, values){
     if(!values.email){
         error.email = toast.error("Email Required...!");
@@ -71,11 +94,4 @@ function emailVerify(error ={}, values){
     }
 
     return error;
-}
-
-// validate profile page
-export async function profileValidation(values){
-    const errors = emailVerify({}, values);
-
-    return errors;
 }
