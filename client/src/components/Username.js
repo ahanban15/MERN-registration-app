da3bc3d3155c316a5013ddfb81f07a2b91ed9a24
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-01-26 13:55:36 +0530

    store
    zustand

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -5,6 +5,7 @@ import { Toaster} from 'react-hot-toast'
 import { useFormik } from 'formik'
 import styles from '../styles/Username.module.css'
 import { usernameValidate } from '../helper/validate'
+import { useAuthStore } from '../../store/store'
 
 export const Username = () => {
   const formik = useFormik({
